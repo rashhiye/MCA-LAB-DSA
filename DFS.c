dda140c29cf2e18bf474635f899c583cedad2c16
Muhammed Rashid KP <rashidkp2004@gmail.com>
2025-11-07 10:29:07 +0530

    Create DFS.c

---
+++ DFS.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#define MAX_NODES 100
+
+
+void DFS(int n, int graph[n][n], int visited[], int source) {
+    printf("%d ", source);
+    visited[source] = 1;
+    // For each node adjacent to u
+    for (int i = 0; i < n; i++) {
+        if (graph[source][i] != 0) {
+            // If the node is not already visited
+            if (!visited[i])
+                DFS(n, graph, visited, i);
+        }
+    }
+}
+
+void main()
+{
+    int n;
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+
+    // Visited Array
+    int V[n];
+    for (int i = 0; i < n; i++) {
+        V[i] = 0;
+    }
+
+    // Adjacency matrix
+    int A[n][n];
+    int cost = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("A[%d][%d]: ", i, j);
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    // DFS 
+    // (A Stack works in the background of the recursive algorithm)
+    int source;
+    printf("Enter the starting node: ");
+    scanf("%d", &source);
+    if (source < 0 || source >= n) {
+        printf("Invalid!\n");
+        return;
+    }
+    printf("DFS -> ");
+    DFS(n ,A , V, source);
+    printf("\n");
+}
